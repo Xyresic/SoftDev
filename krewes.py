125bf388252f8c6ff29fc2a437df381d1ad65ed8
Eric Lam <43048491+Xyresic@users.noreply.github.com>
2019-09-10 18:26:58 -0400

    Updated dictionary

--- krewes.py
+++ krewes.py
@@ -3,7 +3,9 @@ def printRandFromDict(dictionary):
     keys = list(dictionary.keys())
     team = dictionary[keys[random.randrange(len(keys))]]
     print(team[random.randrange(len(team))])
-KREWES = {'orpheus':['Emily','Kevin'],
-    'rex':['William','Joseph'],
-    'endymion':['Grace','Nahi']}
+KREWES = {
+	'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie ', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+	'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+	'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
+}
 printRandFromDict(KREWES)
