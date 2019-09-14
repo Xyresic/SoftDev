eed4ebcbc493b056599116d0ddcecc7f96ca9795
ericlau00 <sharkguy418@gmail.com>
2019-09-14 13:20:26 -0400

    comments

--- 03_csv/MortyiestRick-lamE-lauE.py
+++ 03_csv/MortyiestRick-lamE-lauE.py
@@ -1,34 +1,54 @@
 import random
 
+"""
+-------------------------------------------------------------------
+-------------------------CSV TO DICTIONARY-------------------------
+-------------------------------------------------------------------
+"""
+
 dictionary = {}
 
-#turn the csv into a list, making each item in the list a job,percentage
+# turn the csv into a list, making each item in the list a job,percentage
 data = open("./occupations.csv").read().rstrip("\n").split("\n")
 
-#remove the first and last items which are ["Job Class, Percentage"] and ["Total, 99.8"]
+# remove the first and last items which are ["Job Class, Percentage"] and ["Total, 99.8"]
 data = data[1:len(data) - 1]
 
-#for each item in the data list
+# for each item in the data list, split the item by comma,
+# and add the job/percentage as a key/value pair to the dictionary 
 for item in data:
-
-    # split the item by comma
     splits = item.rsplit(",", 1)
-
-    #add the job as a key to the dictionary and assign the percentage as the value 
     dictionary[splits[0].strip("\"")] = float(splits[1])
 
-#returns a weighted random element from a dictionary
+"""
+-------------------------------------------------------------------
+-------------------------SELECT RANDOM JOB-------------------------
+-------------------------------------------------------------------
+"""
+
+# returns a weighted random element from a dictionary
 def weightedRandFromDict(dictionary):
     keys = list(dictionary.keys())
     weights = list(dictionary.values())
-    return random.choices(keys,weights=weights,k=1)[0]
+
+    # return one(k=1) random job accounting for attached weights 
+    # the [0] is needed because random.choices returns a list 
+    return random.choices(keys,weights=weights,k=1)[0] 
+
+"""
+-------------------------------------------------------------------
+--------------------------TESTING RESULTS--------------------------
+-------------------------------------------------------------------
+"""
 
 results = []
 tests = 100000
 
+# for test number of times, append a random job to the results list 
 for i in range(tests):
     results.append(weightedRandFromDict(dictionary))
 
+# for each job, print the actual percentage of the job and
+# print the count of the number of appearances of job in the results array as a percent out of tests
 for job in list(dictionary.keys()):
-    print(job)
-    print("actual: "+str(dictionary[job])+" experimental: "+str(results.count(job)*100.0/tests), end="\n\n")
+    print(job, "\n", "actual:", str(dictionary[job]), "experimental:", str(results.count(job)*100.0/tests), end="\n\n")
