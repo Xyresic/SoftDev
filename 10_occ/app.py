4f936769abb9394a4a468da4eee32e311eac533b
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-09-23 08:04:49 -0400

    fixed sh/bsh bug?

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -7,8 +7,8 @@ from random import choices
 from flask import Flask, render_template
 import csv
 app = Flask(__name__)
-__ppath__ = __file__[:__file__.rfind('/')] # python file's parent directory
-
+__ppath__ = __file__[: __file__.rfind('/') if __file__.rfind('/') != -1 else __file__.rfind('\\')] # python file's parent directory
+print(__ppath__)
 def weightedRandFromDict(dictionary):
     items = dictionary.items()
     keys = [item[0] for item in items]
@@ -17,8 +17,8 @@ def weightedRandFromDict(dictionary):
 
 def gen_dict():
     lines = [line for line in csv.reader(open(__ppath__ + "/data/occupations.csv"))] # uses a csv.reader to parse the file
-    lines = [(line[0],float(line[1])) for line in lines[1:-2]]
-    lines.append(("Unemployed",0.2)) # removes the column names and "Total" row, re-expresses as a list of tuples to enable dictionary conversion
+    lines = [(line[0],float(line[1])) for line in lines[1:-2]]# removes the column names and "Total" row, re-expresses as a list of tuples to enable dictionary conversion
+    lines.append(("Unemployed",0.2)) # accounts for missing 0.2% of jobs
     return dict(lines)
 
 @app.route("/")
