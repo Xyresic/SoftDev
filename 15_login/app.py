97d6c8d9fc071d1d029c09bc87bc1b119f89b9ce
Eric Lam <elam00@stuy.edu>
2019-10-03 18:17:13 -0400

    Fixed Website

--- 15_login/app.py
+++ 15_login/app.py
@@ -9,40 +9,43 @@ from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-@app.route("/", methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/welcome', methods=['GET','POST'])
+def welcome():
+    return render_template('welcome.html', user=session['user'])
+
+@app.route('/error', methods=['GET','POST'])
+def error():
+    user = session.pop('user') != 'Michael'
+    pword = session.pop('pass') != 'hunter2'
+    error = 'Bad Juju' if user and pword else 'Bad Username' if user else 'Bad Password'
+    return render_template('error.html', message=error)
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    session.pop('user')
+    session.pop('pass')
+    return render_template('logout.html')
+
+@app.route('/', methods=['GET','POST'])
 def rootFunc():
-    if request.cookies.get('logout')=='Log Out':
+    if 'user' in session:
         return redirect(url_for('welcome'))
     else:
         return redirect(url_for('login'))
 
-@app.route('/login', methods=['POST'])
-def login():
-    return render_template('login.html')
-
-@app.route("/check", methods=['POST'])
+@app.route('/check', methods=['GET','POST'])
 def authenticate():
-    if request.form["username"] == "Michael" and request.form["password"] == "hunter2":
-        session['user'] = 'Michael'
-        return render_template('welcome.html', user=request.form["username"])
+    session['user'] = request.form['username']
+    session['pass'] = request.form['password']
+    if session['user'] == 'Michael' and session['pass'] == 'hunter2':
+        return redirect(url_for('welcome'))
     else:
         return redirect(url_for('error'))
 
-@app.route('/welcome', methods=['POST'])
-def welcome():
-    return render_template('welcome.html')
-
-@app.route('/error', methods=['POST'])
-def error():
-    user = request.form["username"] != "Michael"
-    pword = request.form["password"] != "hunter2"
-    error = 'Bad Juju' if user and pword else 'Bad Username' if user else 'Bad Password'
-    return render_template('error.html', message=error)
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    return render_template('logout.html')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.debug = True
     app.run()
