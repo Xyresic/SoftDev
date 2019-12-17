39b3abd7ff02933d11ddcb55718b8a1a5b61922b
Xyresic <ericjmlam@gmail.com>
2019-12-16 21:29:48 -0500

    Snazzy

--- 29_xtra/dommy.js
+++ 29_xtra/dommy.js
@@ -1,63 +1,88 @@
-//Team SciTal - Eric Lam & David Xideng
+//Team SciTal - Eric Lam & David Xiedeng
 //SoftDev1 pd1
 //K29 -- Witch Season
 //2019-12-14
 
-var changeHeading = function(e) {
+var changeHeading = function (e) {
     var h = document.getElementById("h");
     h.innerHTML = e;
     //console.log(e);
 };
 
 
-var removeItem = function(e) {
-    e.remove();
+var removeItem = function (e) {
+    //console.log(e.getAttribute == "fib");
+    if (e.getAttribute("type") == "fib") {
+        deFib = fiblis.pop()
+        console.log(deFib);
+        deFib.remove();
+    } else {
+        e.remove();
+        items--;
+    }
 };
 
 
 const lis = document.getElementsByTagName("li");
 
-for (let i = 0; i < lis.length; i++) {
-    let item = lis[i];
+var makeDeletable = (item) => {
     item.addEventListener("mouseover", () => {
         changeHeading(item.innerHTML);
-        //console.log(i);
     });
-    //console.log(lis[i]);
-    item.addEventListener("mouseout", () => {changeHeading("Hello World")});
-    item.addEventListener("click", () => {removeItem (item)});
-};
+    item.addEventListener("mouseout", () => {
+        changeHeading("Hello World")
+    });
+    item.addEventListener("click", () => {
+        removeItem(item)
+    });
+}
 
-var addItem = function(e) {
-    var list = document.getElementsByTagName("ol")[0];
-    var item = document.createElement("li");
-    item.innerHTML = "WORD";
-    item.addEventListener('mouseover', () => {changeHeading('WORD')});
-    item.addEventListener("mouseout", () => {changeHeading("Hello World")});
-    item.addEventListener("click", () => {removeItem (item)});
-    //console.log(item);
+for (let i = 0; i < lis.length; i++) {
+    let item = lis[i];
+    makeDeletable(item);
+    items++;
+}
+;
+
+var items = 8;
+
+var addItem = function (e) {
+    list = document.getElementsByTagName("ol")[0];
+    item = document.createElement("li");
+    item.innerHTML = "item " + items;
+    items++;
+    makeDeletable(item);
     list.appendChild(item);
 };
 
 var button = document.getElementById("b")
 button.addEventListener('click', addItem)
 
-var fib = function(n) {
+var fiblis = [];
+
+var fib = function (n) {
     if (n < 2) {
         return 1;
-    }
-    else {
-        return fib(n-1)+fib(n-2);
+    } else {
+        let n1 = parseInt(fiblis[n - 1].innerHTML);
+        let n2 = parseInt(fiblis[n - 2].innerHTML);
+        return n1 + n2;
     }
 };
 
+
 var addFib = function (e) {
-    var list = document.getElementsByTagName("ol")[1];
-    var n = list.getElementsByTagName('li').length;
-    var li = document.createElement('li');
-    li.innerHTML = fib(n);
-    list.appendChild(li);
-};
+    let list = document.getElementsByTagName("ol")[1];
+    let currentFib = fib(fiblis.length);
+    //console.log(currentFib);
+    let item = document.createElement("li");
+    item.setAttribute("type", "fib");
+    item.innerHTML = currentFib;
+    makeDeletable(item);
+    list.appendChild(item);
+    fiblis.push(item);
+    //console.log(fiblis);
+}
 
 var fb = document.getElementById("fb");
 fb.addEventListener("click", addFib);
