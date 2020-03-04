572f5ed1390ee0539e18347a541d64dc8a3f0963
Eric Lam <elam00@stuy.edu>
2020-03-03 21:44:50 -0500

    Finished Work

--- 10_mongo/query.py
+++ 10_mongo/query.py
@@ -4,4 +4,43 @@
 #2020-03-03
 
 from pymongo import MongoClient
+from bson.json_util import loads
 client = MongoClient('localhost')
+db = client.sd10
+col = db.southpark
+
+'''
+data = []
+doc = ''
+with open('southpark.json','r') as f:
+    for c in f.read():
+        if '}}}' in doc:
+            data.append(doc)
+            doc = ''
+        else:
+            doc += c
+    data.append(doc)
+for d in data:
+    col.insert_one(loads(d))
+'''
+
+def filter_id(id):
+    return list(col.find({'id':id}))
+
+def filter_title(name):
+    return list(col.find({'name':name}))
+
+def filter_season_num(season,num):
+    return list(col.find({'season':season,'number':num}))
+
+def filter_before_date(date):
+    return list(col.find({'airdate':{'$lte':date}}))
+
+def filter_desc(desc):
+    return list(col.find({'summary':{'$regex':'(?i).*%s.*' % desc.lower()}}))
+
+#print(filter_id(7919))
+#print(filter_title('Cartman Gets an Anal Probe'))
+#print(filter_season_num(1,1))
+#print(filter_before_date('2010-06-27'))
+#print(filter_desc('manbearpig'))
