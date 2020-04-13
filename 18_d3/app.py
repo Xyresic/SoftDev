06767c7297ff84bea76b23499c4b949d3315919e
Eric Lam <elam00@stuy.edu>
2020-04-13 14:35:30 -0400

    Map Rendering

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def root():
+    return render_template('main.html')
+
+
+@app.route('/data')
+def data():
+    return jsonify(get_data())
+
+
+def get_data():
     return ''
+
 
 if __name__ == '__main__':
     app.debug = True
---
+++ 18_d3/static/transition.js
@@ -0,0 +1,35 @@
+let rend_btn = document.getElementById('render');
+let trans_btn = document.getElementById('transition');
+
+let json_url = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-10m.json';
+
+let width = 1000;
+let height = 500;
+
+let proj = d3.geoEqualEarth()
+            .scale(width/2/Math.PI)
+            .center([0,0])
+            .translate([width/2, height/2]);
+let path = d3.geoPath(proj);
+
+let map = d3.select('#map').append('svg')
+            .attr('viewBox', [0, 0, width, height]);
+
+let group = map.append('g')
+                .selectAll('path');
+
+let render = () => {
+    d3.json(json_url).then((data) => {
+        let countries = topojson.feature(data, data.objects.countries)
+        group.data(countries.features)
+            .join('path')
+            .attr('d', path);
+    });
+};
+
+rend_btn.addEventListener('click', render);
+
+
+
+
+
