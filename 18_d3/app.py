ac5a2d1cc22a193cd8c351292da52eab44c07035
Eric Lam <elam00@stuy.edu>
2020-04-13 20:09:51 -0400

    coloring

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -1,15 +1,52 @@
-import os
-from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
+import os, csv
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+rename = {
+    "Antigua and Barbuda":"Antigua and Barb.",
+    "Bosnia and Herzegovina":"Bosnia and Herz.",
+    "Central African Republic":"Central African Rep.",
+    "Congo (Brazzaville)":"Congo",
+    "Congo (Kinshasa)":"Dem. Rep. Congo",
+    "Dominican Republic":"Dominican Rep.",
+    "Equatorial Guinea":"Eq. Guinea",
+    "Korea, South":"South Korea",
+    "Saint Kitts and Nevis":"St. Kitts and Nevis",
+    "Saint Vincent and the Grenadines":"St. Vin. and Gren.",
+    "Sao Tome and Principe":"São Tomé and Principe",
+    "South Sudan":"S. Sudan",
+    "North Macedonia":"Macedonia",
+    "US":"United States of America"
+}
+
 def get_data():
-    return ''
+    parse = {}
+    with open('./static/data/covid19_confirmed.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        next(reader)
+        for row in reader:
+            date = row[4]
+            if date not in list(parse):
+                parse[date] = {}
+            name = rename[row[1]] if row[1] in list(rename) else row[1]
+            if name in list(parse[date]):
+                parse[date][name] += int(row[5])
+            else:
+                parse[date][name] = int(row[5])
+    return parse
+
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template('main.html', data = get_data())
+    return render_template('main.html', data=get_data())
+
+
+@app.route('/data')
+def data():
+    return jsonify(get_data())
 
 
 if __name__ == '__main__':
--- 18_d3/static/transition.js
+++ 18_d3/static/transition.js
@@ -2,9 +2,9 @@ let rend_btn = document.getElementById('render');
 let trans_btn = document.getElementById('transition');
 
 let json_url = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-10m.json';
-
 let width = 1000;
 let height = 500;
+let date = new Date('2020-01-22');
 
 let proj = d3.geoEqualEarth()
             .scale(width/2/Math.PI)
@@ -14,26 +14,32 @@ let path = d3.geoPath(proj);
 
 let map = d3.select('#map').append('svg')
             .attr('viewBox', [0, 0, width, height]);
-
 let group = map.append('g')
                 .selectAll('path');
 
+let data_full = d3.json('/data').then(d => {data_full = d});
+
+let color = d3.scaleSequential()
+                .domain([0,600000])
+                .interpolator(d3.interpolateRgbBasis(['#ccc','red','black']))
+                .unknown('#ccc');
+
 let render = () => {
-    d3.json(json_url).then((data) => {
+    d3.json(json_url).then(data => {
         let countries = topojson.feature(data, data.objects.countries)
         group.data(countries.features)
             .join('path')
-            .attr('d', path);
+                .attr('d', path)
+                .attr('fill', d => color(data_full[date][d.properties.name]));
     });
 };
 
-let data = Object.assign()
+let advance = () => {
 
-//let color = d3.scaleSequential()
-//                .domain()
+};
 
 rend_btn.addEventListener('click', render);
-
+trans_btn.addEventListener('click', advance);
 
 
 
